d2b2fd5474dae5c3d1cc95567c34cac119aee38e
jeonbyeongmin <qudals7613@gmail.com>
2022-12-21 17:34:03 +0900

    feat: echo

---
+++ echor/src/main.rs
@@ -0,0 +1,27 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Byeongmin Jeon <qudals7613@gmail.com>")
+        .about("Echoes the input back to the user")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input Text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+}
